bd0bf7bc8ffbe91590c8b22f7b392db964155bef
Anish Ghimire <anish.ghimire001@gmail.com>
2021-07-10 23:33:03 +0545

    Displays Name and info, and all repositories via Github api, also made minimal and clean design

--- js/script.js
+++ js/script.js
@@ -1,25 +1,65 @@
-header = (data) => {
-  document.getElementById("profileImage").src = data['avatar_url']
-  document.getElementById("profileName").textContent = data['name']
-  document.getElementById("profileBio").textContent = data['bio']
-  document.getElementById("profileID").textContent = data['login']
-  document.getElementById("profileID").href = data['html_url']
+header = (profileData) => {
+  document.getElementById("profileImage").src = profileData['avatar_url']
+  document.getElementById("profileName").textContent = profileData['name']
+  document.getElementById("profileBio").textContent = profileData['bio']
+  document.getElementById("profileID").textContent = profileData['login']
+  document.getElementById("profileID").href = profileData['html_url']
 }
 
-displayRepos = (data) => {
+createRepo = (repoInfo) => {
+  let project = document.getElementById('projects-container')
+  let repoDiv = document.createElement('div')
+  repoDiv.className = "repo-wrapper clearfix"
+  let repoLink = document.createElement('a')
+  repoDiv.appendChild(repoLink)
+  repoLink.href = repoInfo['html_url']
+  
+  projectTitle = document.createElement('h3')
+  projectTitle.className = "projectTitle"
+  projectTitle.textContent = repoInfo['name']
+  repoLink.appendChild(projectTitle)
 
+  projectDesc = document.createElement('p')
+  projectDesc.textContent = repoInfo['description']
+  projectDesc.className = "projectDesc"
+  repoLink.appendChild(projectDesc)
+
+  fetch(repoInfo["languages_url"])
+  .then(response => response.json())
+  .then(function(lang) {
+    langs_used = Object.keys(lang) 
+    if (langs_used.length > 0) {
+      projectLang = document.createElement('p')
+      projectLang.textContent = "🔶 " + langs_used.join(', ')
+      projectLang.className = "projectLang"
+      repoLink.appendChild(projectLang)
+    }
+  })
+
+  if (repoInfo["stargazers_count"] > 0) {
+    projectStars = document.createElement('p')
+    projectStars.textContent = "⭐ " + repoInfo["stargazers_count"]
+    projectStars.className = "projectStars"
+    repoLink.appendChild(projectStars)
+  }
+
+  project.appendChild(repoDiv)
+}
+
+displayRepos = (reposData) => {
+  for (let i=0; i<reposData.length; i++) {
+    createRepo(reposData[i])
+  }
 }
 
 fetch('https://api.github.com/users/anish-g')
   .then(response => response.json())
-  .then(function(data) {
-    console.log(data)
-    header(data)
+  .then(function(profileData) {
+    header(profileData)
   })
 
 fetch('https://api.github.com/users/anish-g/repos')
   .then(response => response.json())
-  .then(function(data) {
-    console.log(data)
-    displayRepos(data)
+  .then(function(reposData) {
+    displayRepos(reposData)
   })
